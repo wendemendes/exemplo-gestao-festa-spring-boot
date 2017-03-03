7a647899f2abfd76fc5250461d3c7171ffa3b19d
Wende Mendes <wende@bluesoft.com.br>
2017-03-03 13:54:48 -0300

    criando estrutura projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# exemplo-gestao-festa-spring-boot
---
+++ src/main/java/com/algaworks/festa/comtroller/ConvidadosController.java
@@ -0,0 +1,25 @@
+package com.algaworks.festa.comtroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.festa.repository.Convidados;
+
+@Controller
+public class ConvidadosController {
+
+	@Autowired
+	private Convidados convidados;
+
+	@GetMapping("/convidados")
+	public ModelAndView listar() {
+
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		modelAndView.addObject("convidados", convidados.findAll());
+		
+		return modelAndView;
+	}
+
+}
